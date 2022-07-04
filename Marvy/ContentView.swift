accebe39229ec6530bc17356ba724442e9c81949
Max Korytko <maxk2005@gmail.com>
2022-07-04 17:17:54 -0400

    Initial commit

---
+++ Marvy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvy
+//
+//  Created by Maksym Korytko  on 7/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marvy/MarvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvyApp.swift
+//  Marvy
+//
+//  Created by Maksym Korytko  on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MarvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
